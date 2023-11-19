b01798e24a849154aa3dde942d6905d24fd2762a
Matt Rogers <42357482+twitchyvr@users.noreply.github.com>
2023-11-19 11:56:27 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
-const formidable = require('formidable');
-const util = require('util');
-const fs = require('fs');
-const { Telemetry } = require('./telemetry');
+const formidable = require('formidable')
+const util = require('util')
+const fs = require('fs')
+const { Telemetry } = require('./telemetry')
 
 module.exports = async function (context, req) {
   try {
     // Convert formidable form parsing to a promise
-    const form = new formidable.IncomingForm();
-    const parse = util.promisify(form.parse).bind(form);
+    const form = new formidable.IncomingForm()
+    const parse = util.promisify(form.parse).bind(form)
 
     // Parse the form
-    const { fields, files } = await parse(req);
+    const { fields, files } = await parse(req)
 
     // Get uploaded file
-    const uploadedFile = files.ibtFile;
+    const uploadedFile = files.ibtFile
 
     // Ensure file exists
     if (!uploadedFile) {
-      throw new Error('No file uploaded.');
+      throw new Error('No file uploaded.')
     }
 
     // Read file
-    const telemetryData = fs.readFileSync(uploadedFile.filepath);
+    const telemetryData = fs.readFileSync(uploadedFile.filepath)
 
     // Process telemetry
-    const telemetry = new Telemetry(telemetryData);
-    console.log(telemetry.header);
+    const telemetry = new Telemetry(telemetryData)
+    console.log(telemetry.header)
 
     // Response
     context.res = {
       status: 200,
       body: 'Telemetry data processed.'
-    };
+    }
   } catch (error) {
     context.res = {
       status: 500,
       body: `Error: ${error.message}`
-    };
+    }
   }
-};
+}
