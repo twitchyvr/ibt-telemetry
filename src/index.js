b77fc0b0613eb4a89edb97839152143116f2472a
Matt Rogers <42357482+twitchyvr@users.noreply.github.com>
2023-11-19 01:52:08 -0700

    updated modules and fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /**
  * iRacing ibt telemetry parser.
- 
+
 import Telemetry from './telemetry'
 import Sample from './telemetry-sample'
 import readFileToBuffer from './utils/read-file-to-buffer'
@@ -18,43 +18,44 @@ export {
 */
 
 // You might need to install and use a library like formidable to parse multipart/form-data
-const formidable = require('formidable');
+const fs = require('fs')
+const formidable = require('formidable')
 
-const { Telemetry } = require('./telemetry');
+const { Telemetry } = require('./telemetry')
 
 module.exports = async function (context, req) {
-    try {
-        // Parse the multipart/form-data request
-        const formData = await new Promise((resolve, reject) => {
-            const form = new formidable.IncomingForm();
-            
-            form.parse(req, (err, fields, files) => {
-                if (err) reject(err);
-                resolve(files);
-            });
-        });
+  try {
+    // Parse the multipart/form-data request
+    const formData = await new Promise((resolve, reject) => {
+      const form = new formidable.IncomingForm()
 
-        // Assume the file is uploaded with the key 'ibtFile'
-        const uploadedFile = formData.ibtFile;
+      form.parse(req, (err, fields, files) => {
+        if (err) reject(err)
+        resolve(files)
+      })
+    })
 
-        // Read the file content (this step depends on how formidable returns the file)
-        const telemetryData = fs.readFileSync(uploadedFile.filepath);
+    // Assume the file is uploaded with the key 'ibtFile'
+    const uploadedFile = formData.ibtFile
 
-        // Parse telemetry data
-        const telemetry = new Telemetry(telemetryData);
+    // Read the file content (this step depends on how formidable returns the file)
+    const telemetryData = fs.readFileSync(uploadedFile.filepath)
 
-        // Process the telemetry data as needed
-        // ...
+    // Parse telemetry data
+    const telemetry = new Telemetry(telemetryData)
 
-        // Send response
-        context.res = {
-            status: 200,
-            body: "Telemetry data processed."
-        };
-    } catch (error) {
-        context.res = {
-            status: 500,
-            body: `Error: ${error.message}`
-        };
+    // Process the telemetry data as needed
+    console.log(telemetry.header)
+
+    // Send response
+    context.res = {
+      status: 200,
+      body: 'Telemetry data processed.'
     }
-};
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: `Error: ${error.message}`
+    }
+  }
+}
