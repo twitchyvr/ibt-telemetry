41f8b2ceeadac09eb163d17eb70d261333b4000e
Matt Rogers <42357482+twitchyvr@users.noreply.github.com>
2023-11-19 01:38:35 -0700

    updated for function app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /**
  * iRacing ibt telemetry parser.
- */
+ 
 import Telemetry from './telemetry'
 import Sample from './telemetry-sample'
 import readFileToBuffer from './utils/read-file-to-buffer'
@@ -15,3 +15,46 @@ export {
   readFileToBuffer,
   telemetryFileLoader
 }
+*/
+
+// You might need to install and use a library like formidable to parse multipart/form-data
+const formidable = require('formidable');
+
+const { Telemetry } = require('./telemetry');
+
+module.exports = async function (context, req) {
+    try {
+        // Parse the multipart/form-data request
+        const formData = await new Promise((resolve, reject) => {
+            const form = new formidable.IncomingForm();
+            
+            form.parse(req, (err, fields, files) => {
+                if (err) reject(err);
+                resolve(files);
+            });
+        });
+
+        // Assume the file is uploaded with the key 'ibtFile'
+        const uploadedFile = formData.ibtFile;
+
+        // Read the file content (this step depends on how formidable returns the file)
+        const telemetryData = fs.readFileSync(uploadedFile.filepath);
+
+        // Parse telemetry data
+        const telemetry = new Telemetry(telemetryData);
+
+        // Process the telemetry data as needed
+        // ...
+
+        // Send response
+        context.res = {
+            status: 200,
+            body: "Telemetry data processed."
+        };
+    } catch (error) {
+        context.res = {
+            status: 500,
+            body: `Error: ${error.message}`
+        };
+    }
+};
