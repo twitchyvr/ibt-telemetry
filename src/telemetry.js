9229e589d208ff076f5a38f6657d9cf61df9650f
Matt Rogers <42357482+twitchyvr@users.noreply.github.com>
2023-11-19 09:45:21 -0700

    Update telemetry.js

--- src/telemetry.js
+++ src/telemetry.js
@@ -1,79 +1,54 @@
-import TelemetrySample from './telemetry-sample'
-import telemetryFileLoader from './utils/telemetry-file-loader'
+const TelemetrySample = require('./telemetry-sample');
+const telemetryFileLoader = require('./utils/telemetry-file-loader');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const variableHeaders = new WeakMap();
+const fileDescriptor = new WeakMap();
 
-const fs = require('fs')
-const yaml = require('js-yaml')
-const variableHeaders = new WeakMap()
-const fileDescriptor = new WeakMap()
+class Telemetry {
+  constructor(telemetryHeader, diskSubHeader, sessionInfo, varHeaders, fd) {
+    this.headers = telemetryHeader;
+    this.diskHeaders = diskSubHeader;
+    this.sessionInfo = yaml.safeLoad(sessionInfo);
 
-/**
- * iRacing Telemetry
- */
-export default class Telemetry {
-  /**
-   * Telemetry constructor.
-   */
-  constructor (telemetryHeader, diskSubHeader, sessionInfo, varHeaders, fd) {
-    this.headers = telemetryHeader
-    this.diskHeaders = diskSubHeader
-    this.sessionInfo = yaml.safeLoad(sessionInfo)
-
-    fileDescriptor.set(this, fd)
-    variableHeaders.set(this, varHeaders)
+    fileDescriptor.set(this, fd);
+    variableHeaders.set(this, varHeaders);
   }
 
-  /**
-   * Instantiate a Telemetry instance from the contents of an ibt file
-   *
-   * @param file path to *.ibt file
-   * @return Telemetry instance of telemetry
-   */
-  static fromFile (file) {
-    return telemetryFileLoader(file)
+  static fromFile(file) {
+    return telemetryFileLoader(file);
   }
 
-  /**
-   * Telemetry variable headers.
-   */
-  get varHeaders () {
-    return variableHeaders.get(this)
+  get varHeaders() {
+    return variableHeaders.get(this);
   }
 
-  /**
-   * Generate a unique key for the telemetry session.
-   *
-   * The unique key is a combination of 3 fields:
-   *   accountId-sessionId-subSessionId
-   *
-   * @return string
-   */
-  uniqueId () {
-    const accountId = this.sessionInfo.DriverInfo.Drivers[this.sessionInfo.DriverInfo.DriverCarIdx].UserID
-    const sessionId = this.sessionInfo.WeekendInfo.SessionID
-    const subSessionId = this.sessionInfo.WeekendInfo.SubSessionID
-    return `${accountId}-${sessionId}-${subSessionId}`
+  uniqueId() {
+    const accountId = this.sessionInfo.DriverInfo.Drivers[this.sessionInfo.DriverInfo.DriverCarIdx].UserID;
+    const sessionId = this.sessionInfo.WeekendInfo.SessionID;
+    const subSessionId = this.sessionInfo.WeekendInfo.SubSessionID;
+    return `${accountId}-${sessionId}-${subSessionId}`;
   }
 
-  /**
-   * Telemetry samples generator.
-   */
-  * samples () {
-    let hasSample = true
-    let count = 0
+  * samples() {
+    let hasSample = true;
+    let count = 0;
 
-    const fd = fileDescriptor.get(this)
-    const length = this.headers.bufLen
-    const buffer = Buffer.alloc(length)
+    const fd = fileDescriptor.get(this);
+    const length = this.headers.bufLen;
+    const buffer = Buffer.alloc(length);
 
     while (hasSample) {
-      const start = this.headers.bufOffset + (count++ * length)
-      const bytesRead = fs.readSync(fd, buffer, 0, length, start)
+      const start = this.headers.bufOffset + (count++ * length);
+      const bytesRead = fs.readSync(fd, buffer, 0, length, start);
 
       if (bytesRead !== length) {
-        hasSample = false
+        hasSample = false;
       } else {
-        yield new TelemetrySample(buffer, this.varHeaders)
+        yield new TelemetrySample(buffer, this.varHeaders);
       }
     }
   }
 }
+
+module.exports = Telemetry;
