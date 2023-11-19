d6d41c4d979346adf27b279a69f843f81ffd52e7
Matt Rogers <42357482+twitchyvr@users.noreply.github.com>
2023-11-19 12:08:23 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-const { Telemetry } = require('./telemetry');
+const { Telemetry } = require('./telemetry')
 
 module.exports = async function (context, req) {
   try {
     // Check if there is content in the request body
     if (!req.rawBody) {
-      throw new Error('No file uploaded.');
+      throw new Error('No file uploaded.')
     }
 
     // The binary data of the .ibt file is contained in req.rawBody
-    const telemetryData = req.rawBody;
+    const telemetryData = req.rawBody
 
     // Process telemetry
-    const telemetry = new Telemetry(telemetryData);
-    console.log(telemetry.header);
+    const telemetry = new Telemetry(telemetryData)
+    console.log(telemetry.header)
 
     // Response
     context.res = {
       status: 200,
       body: 'Telemetry data processed.'
-    };
+    }
   } catch (error) {
     context.res = {
       status: 500,
       body: `Error: ${error.message}`
-    };
+    }
   }
-};
+}
